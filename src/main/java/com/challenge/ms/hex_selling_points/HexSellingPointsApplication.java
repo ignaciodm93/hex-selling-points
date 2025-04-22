dc1063c38f45fd157da2a199b5e405cc165ae69b
NachoPC <ignaciodm93@gmail.com>
2025-04-22 17:44:01 -0300

    no message

---
+++ src/main/java/com/challenge/ms/hex_selling_points/HexSellingPointsApplication.java
@@ -0,0 +1,11 @@
+package com.challenge.ms.hex_selling_points;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexSellingPointsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HexSellingPointsApplication.class, args);
+	}
+}
