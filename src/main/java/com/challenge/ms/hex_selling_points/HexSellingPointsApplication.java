51d5fcaa836d2afa1a9abffea80ceb3995d2e37a
NachoPC <ignaciodm93@gmail.com>
2025-04-29 17:32:51 -0300

    dependencias del pom y configuración de eureka (properties y annotation en el run para que se pueda conectar como cliente)

--- src/main/java/com/challenge/ms/hex_selling_points/HexSellingPointsApplication.java
+++ src/main/java/com/challenge/ms/hex_selling_points/HexSellingPointsApplication.java
@@ -2,7 +2,9 @@ package com.challenge.ms.hex_selling_points;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HexSellingPointsApplication {
 	public static void main(String[] args) {
